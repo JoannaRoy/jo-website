db6870a4bde73b235187805ffa2432f9013c5393
Joanna Roy <62259358+JoannaRoy@users.noreply.github.com>
2024-12-26 20:04:10 -0500

    change to hamburger

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,47 +1,136 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 
-const linkStyle = {
-  textDecoration: "none",
-  fontSize: "1rem",
-  transition: "color 0.3s",
-  fontWeight: "bold",
-  color: "black",
-  margin: "0 10px",
+export interface HamburgerProps {
+  onClick: () => void;
+  isInitiallyOpen?: boolean;
+}
+
+const commonStyle = {
+  width: "25px",
+  height: "3px",
+  marginBottom: "6px",
+  transition: "transform 0.4s",
 };
 
-const NavBar: React.FC = () => {
+export function Hamburger(props: HamburgerProps) {
+  const { onClick, isInitiallyOpen } = props;
+  const [isOpen, setIsOpen] = useState<boolean>(isInitiallyOpen ?? false);
+  const hamburgerRef = useRef<HTMLButtonElement>(null);
+
+  const handleClick = () => {
+    setIsOpen((prev) => !prev);
+    onClick();
+  };
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (
+      hamburgerRef.current &&
+      !hamburgerRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  document.addEventListener("click", handleOutsideClick);
+
   return (
-    <nav
-      className="navbar"
+    <button
+      ref={hamburgerRef}
+      onClick={handleClick}
+      type="button"
       style={{
-        display: "flex",
-        height: "2rem",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "1rem 2rem",
-        position: "fixed",
-        top: "20px",
-        left: "20px",
-        right: "20px",
-        zIndex: "1000",
-        borderRadius: "10px",
-        // border: "3px solid white",
-        backgroundColor: "var(--white)",
+        backgroundColor: "transparent",
+        border: "none",
+        zIndex: isOpen ? 1001 : 0,
+        margin: "1rem",
+        position: "absolute",
+        right: 0,
       }}
     >
       <div
         style={{
+          ...commonStyle,
+          backgroundColor: "black",
+          transform: `rotate(${isOpen ? "45deg" : "0"})`,
+          transformOrigin: "left center",
+        }}
+      />
+      <div
+        style={{
+          ...commonStyle,
+          backgroundColor: isOpen ? "transparent" : "black",
+        }}
+      />
+      <div
+        style={{
+          ...commonStyle,
+          backgroundColor: "black",
+          transform: `rotate(${isOpen ? "-45deg" : "0"})`,
+          transformOrigin: "left center",
+        }}
+      />
+    </button>
+  );
+}
+
+const linkStyle = {
+  color: "black",
+  fontWeight: "bold",
+  fontSize: "1rem",
+  textDecoration: "none",
+  margin: "1rem 0",
+};
+
+const NavBar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const navBarRef = useRef<HTMLDivElement>(null);
+
+  const handleHamburgerClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (
+      navBarRef.current &&
+      !navBarRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  document.addEventListener("click", handleOutsideClick);
+
+  return (
+    <div
+      ref={navBarRef}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        position: "fixed",
+        backgroundColor: "transparent",
+        top: 0,
+        right: 0,
+        zIndex: isOpen ? 1000 : 0,
+        transition: "z-index 0.5s",
+      }}
+    >
+      <Hamburger onClick={handleHamburgerClick} isInitiallyOpen={isOpen} />
+      <div
+        style={{
           display: "flex",
-          gap: "10px",
+          zIndex: isOpen ? 1000 : 0,
+          flexDirection: "column",
+          width: "15rem",
           alignItems: "center",
+          backgroundColor: "var(--white)",
+          height: "100vh",
+          boxShadow: "0 4px 8px rgba(0,0,0,0.05)",
+          transform: isOpen ? "translateX(0)" : "translateX(100%)",
+          transition: "transform 0.4s",
         }}
       >
-        <Link to="/" style={linkStyle}>
-          <h2>Joanna</h2>
-        </Link>
-      </div>
-      <div className="navbar-links">
+        <div style={{ height: "3rem" }}></div>
         <Link to="/" style={linkStyle}>
           Home
         </Link>
@@ -61,7 +150,7 @@ const NavBar: React.FC = () => {
           More
         </Link>
       </div>
-    </nav>
+    </div>
   );
 };
 
